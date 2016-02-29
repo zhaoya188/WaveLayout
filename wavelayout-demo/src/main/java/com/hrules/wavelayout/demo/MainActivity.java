37b980fd757e6ed65a83c895b33783ec7bcaee83
Hector de Isidro <hector.isidro@mobusi.com>
2016-02-29 15:15:44 +0100

    Reformat project

--- wavelayout-demo/src/main/java/com/hrules/wavelayout/demo/MainActivity.java
+++ wavelayout-demo/src/main/java/com/hrules/wavelayout/demo/MainActivity.java
@@ -6,25 +6,21 @@ import android.view.View;
 import com.hrules.wavelayout.WaveLayout;
 
 public class MainActivity extends AppCompatActivity {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final WaveLayout waveLayout = (WaveLayout) findViewById(R.id.waveLayout);
-        waveLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (waveLayout.getDirection() == WaveLayout.DIRECTION_OUTWARDS) {
-                    waveLayout.setStrokeWidth(4f);
-                    waveLayout.setDirection(WaveLayout.DIRECTION_INWARDS);
-
-                } else {
-                    waveLayout.setStyle(WaveLayout.STYLE_FILL);
-                    waveLayout.setDirection(WaveLayout.DIRECTION_OUTWARDS);
-                }
-            }
-        });
-    }
+    final WaveLayout waveLayout = (WaveLayout) findViewById(R.id.waveLayout);
+    waveLayout.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        if (waveLayout.getDirection() == WaveLayout.DIRECTION_OUTWARDS) {
+          waveLayout.setStrokeWidth(4f);
+          waveLayout.setDirection(WaveLayout.DIRECTION_INWARDS);
+        } else {
+          waveLayout.setStyle(WaveLayout.STYLE_FILL);
+          waveLayout.setDirection(WaveLayout.DIRECTION_OUTWARDS);
+        }
+      }
+    });
+  }
 }
